a9ebdc1a56e231ae835c82d3837439d90b901027
orregevcode <64591559+orregevcode@users.noreply.github.com>
2022-05-01 13:53:15 +0300

    pus

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // cards array holds all cards
 let card = document.getElementsByClassName("card");
-let cards = [...card];
+let cards = [...card]
+console.log(cards);
 
 // deck of all cards in game
 const deck = document.getElementById("card-deck");
@@ -52,10 +53,6 @@ document.body.onload = startGame();
 
 // @description function to start a new play 
 function startGame(){
- 
-    // empty the openCards array
-    openedCards = [];
-
     // shuffle deck
     cards = shuffle(cards);
     // remove all exisiting classes from each card
